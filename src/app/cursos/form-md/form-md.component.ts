ebe6e31472ae153e3feda6a811bd29dae7e377a1
eiglesia <eiglesia@aubay.es>
2019-07-28 23:54:06 +0200

    Reactive Forms

--- src/app/cursos/form-md/form-md.component.ts
+++ src/app/cursos/form-md/form-md.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ICurso, ITurno } from 'src/app/models/alumno.model';
+import { ICurso, ITurno, AlumnoModel } from 'src/app/models/alumno.model';
 import { CURSOS, TURNOS } from 'src/app/models/datos';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'aby-form-md',
@@ -11,12 +12,36 @@ export class FormMdComponent implements OnInit {
 
   cursos: Array<ICurso>
   turnos: Array<ITurno>
+  alumno: AlumnoModel
+  formAlumno: FormGroup
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.cursos = CURSOS
     this.turnos = TURNOS
+
+    this.formAlumno = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      dni: [''],
+      email: ['@aubay.com', [Validators.required, Validators.email ] ],
+      nacim: [],
+      coment: [],
+      isOk: ['', Validators.required],
+      turno: ['', Validators.required],
+      curso: ['', Validators.required]
+    })
+
+    this.alumno = new AlumnoModel()
+  }
+
+  onSubmit(ev){
+    console.log(ev)
+    console.log('Enviando...')
+    // ev.preventDefault() //Angular lo hace por defecto
+    this.alumno = this.formAlumno.value
+    console.log(this.alumno)
   }
 
 }
